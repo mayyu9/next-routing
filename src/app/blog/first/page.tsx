265b6dca065bdbd0d60498c12ec8540c9fd081e5
mayyu9 <coolmahimsd@gmail.com>
2024-10-12 16:15:14 +0530

    nested routing

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+export default function FirstPage() {
+    return(
+        <h1>Welcome First Page!!!!</h1>
+    )
+};
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,5 @@
+export default function SecondPage() {
+    return(
+        <h1>Welcome Second Page!!!!</h1>
+    )
+};
