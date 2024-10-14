7d84b64fc27f77f6fddc31069b17e8e3c8bd86ce
mayyu9 <coolmahimsd@gmail.com>
2024-10-14 16:35:29 +0530

    static metadata page level

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata:Metadata = {
+    title: "About page"
+  };
+  
+
 export default function AboutPage() {
     return(
         <h1>About page!!!!</h1>
