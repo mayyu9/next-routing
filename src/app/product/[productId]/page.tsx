038c57c7bc2b31756d1278c0c2348239e9463cce
mayyu9 <coolmahimsd@gmail.com>
2024-10-14 17:11:51 +0530

    dynamic metadata page level

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,8 +1,20 @@
-export default function ProductDetailedPage({ params }: {
+// dynamic metadata implementation start
+import { Metadata } from "next";
+
+type props = {
     params : {
         productId: string
     }
-}) {
+};
+
+export const generateMetadata = ({ params }: props): Metadata => {
+    return {
+        title: `Product ${params.productId}`
+    }
+};
+// dynamic metadata implementation end
+
+export default function ProductDetailedPage({ params }: props) {
     return(
         <h1>Product detail page for product id {params.productId} </h1>
     )
