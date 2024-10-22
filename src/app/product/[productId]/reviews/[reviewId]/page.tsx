97d121cb0c1c00ad0711b5e71330de097657e23e
mayyu9 <coolmahimsd@gmail.com>
2024-10-22 11:49:35 +0530

    error scenario dev env

--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,20 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetailPage({ params }: {
     params : {
         productId: string,
         reviewId: string
     }
 }){
+    const count = getRandomInt(2);
+
+    if(count === 1) {
+        throw new Error(' error while loading the page');
+    }
     if(parseInt(params.reviewId) >1000) {
         notFound();
     }
