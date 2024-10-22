c562652eef8afb8d0a3047f3376ad6da80fc11b9
mayyu9 <coolmahimsd@gmail.com>
2024-10-22 13:41:22 +0530

    error in layout

--- src/app/product/[productId]/layout.tsx
+++ src/app/product/[productId]/layout.tsx
@@ -1,9 +1,20 @@
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ProductDetailsLayout({
   children,
 }:{
   children: React.ReactNode;
 }) {
+
+  const count = getRandomInt(2);
+
+  if(count === 1) {
+      throw new Error(' error while loading the products page');
+  }
+
   return (
    <>
         {children}
