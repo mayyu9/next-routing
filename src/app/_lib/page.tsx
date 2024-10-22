c7a01c9394ade16ced37401d7655f7584ffc4f3e
mayyu9 <coolmahimsd@gmail.com>
2024-10-22 13:00:28 +0530

    error scenario fixed both prod n dev env

--- src/app/_lib/page.tsx
+++ src/app/_lib/page.tsx
@@ -1,3 +1,3 @@
 export default function PrivatePage() {
-    return <h1>this is private page can't be accessible vi routes</h1>
+    return <h1>this is private page can&apos;t be accessible vi routes</h1>
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
 import BarChartComponent from '../../components/BarChart';
 
-function BarChart(){
-    return <h1>Bar chart !!!</h1>
-};
+// function BarChart(){
+//     return <h1>Bar chart !!!</h1>
+// };
 
 export default function DashBoard() {
     return (
