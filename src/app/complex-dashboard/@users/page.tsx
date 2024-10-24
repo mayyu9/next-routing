c977dda5f542a06a8ab0ea80f5ce94ee8ff2308b
mayyu9 <coolmahimsd@gmail.com>
2024-10-24 17:48:45 +0530

    parallel routes

---
+++ src/app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,5 @@
+import Card from "@/components/Card";
+
+export default function ComplexUsersDashboard(){
+    return( <Card>users component!!</Card>)
+};
---
+++ src/app/complex-dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function ComplexDashboard(){
+    return( <h1>complex dashboard component!!</h1>)
+};
